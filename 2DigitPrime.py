60213401ea01325eb619851156c3ead5f31d0e24
Kainatfatima711 <kainatfetima@gmail.com>
2026-01-24 13:11:29 +0500

    Add files via upload

---
+++ 2DigitPrime.py
@@ -0,0 +1,21 @@
+print("Finding all 2-digit Prime Numbers\n")
+
+for num in range(10, 100):
+
+    is_prime = True  
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, "is a 2-digit prime number")
+
+
+
+
+
+
+
+
