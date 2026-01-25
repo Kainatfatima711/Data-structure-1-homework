67e433c1d0daca425c12546bbb1d0293620316d9
Kainatfatima711 <kainatfetima@gmail.com>
2026-01-25 13:12:16 +0500

    Add files via upload

---
+++ LoopTime.py
@@ -0,0 +1,21 @@
+def loop_time_complexity():
+    print("=== Loop Time Complexity Project ===\n")
+
+    print("First Loop:")
+    print("for i in range(0, n+1)")
+    print("Time Complexity: O(n)\n")
+
+    print("Second Loop:")
+    print("while (j <= n+1) and j = j * 2")
+    print("Time Complexity: O(log n)\n")
+
+    print("Third Loop:")
+    print("for i in range(0, 100)")
+    print("Time Complexity: O(1)\n")
+
+    print("=== Total Time Complexity ===")
+    print("O(n)  (Dominant term)")
+
+
+# Run Project
+loop_time_complexity()
